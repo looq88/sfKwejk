52b9cf276788c15eb0a506334cfcb052f0014b72
luq <luq@example.com>
2014-10-25 16:37:10 +0200

    bundle i emcje dodanie

---
+++ src/Kwejk/MemsBundle/Entity/Rating.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Kwejk\MemsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Rating
+ *
+ * @ORM\Table(name="rating")
+ * @ORM\Entity
+ */
+class Rating
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Kwejk\UserBundle\Entity\User", inversedBy="ratings")
+     * @ORM\JoinColumn(name="created_by", referencedColumnName="id")
+     */
+    private $createdBy;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Mem", inversedBy="ratings")
+     * @ORM\JoinColumn(name="mem_id", referencedColumnName="id")
+     * @var Mem
+     */
+    private $mem;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="create_at", type="datetime")
+     */
+    private $createAt;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="rating", type="smallint")
+     */
+    private $rating;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createAt
+     *
+     * @param \DateTime $createAt
+     * @return Rating
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * Set rating
+     *
+     * @param integer $rating
+     * @return Rating
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    /**
+     * Get rating
+     *
+     * @return integer 
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+}
