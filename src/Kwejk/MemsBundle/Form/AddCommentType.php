57736d1ef71a11c3ca6056f6e81f50f2a04de297
luq <luq@example.com>
2014-10-27 12:20:43 +0100

    added pagination,menu start, add image, add comment, admin Mem

---
+++ src/Kwejk/MemsBundle/Form/AddCommentType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Kwejk\MemsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AddCommentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('comment', 'textarea', array(
+                'label' => false,
+                'attr' => array('placeholder' => "Treśc komentarza")
+                
+            ))
+            ->add('save', 'submit',array(
+                'label' => 'Dodaj Komentarz'
+            ))
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Kwejk\MemsBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'kwejk_memsbundle_comment';
+    }
+}
